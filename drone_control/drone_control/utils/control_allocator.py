4fe009f7598a32279a2330aa908528770f236ca7
kay <kay1216@yonsei.ac.kr>
2025-11-05 15:26:20 +0900

    control allocation -- relaxed version added

--- drone_control/drone_control/utils/control_allocator.py
+++ drone_control/drone_control/utils/control_allocator.py
@@ -1,12 +1,14 @@
 import numpy as np
 
-class InverseDynamics:
+class ControlAllocator:
     def __init__(self, Param):
         l = Param['arm_length']
         self.C_T = Param['rotor_const']
         k_m = Param['moment_const']
         self.T_max = Param['T_max']
         self.T_min = Param['T_min']
+        self.acc_max = Param['acc_max']
+        self.acc_min = Param['acc_min']
 
         cos_pi_3 = np.cos(np.pi/3)
         sin_pi_3 = np.sin(np.pi/3)
@@ -37,11 +39,25 @@ class InverseDynamics:
         self.K_inv = np.linalg.pinv(K_forward)
 
     def compute_des_rpm(self, f, M):
-        fm = np.hstack([f,M])
-        # print(fm)
-        rotors_thrust = self.K_inv @ fm
+        u = np.hstack([f,M])
+        rotors_thrust = self.K_inv @ u
         rotors_thrust = self.clamp(rotors_thrust)
         rotors_speed = np.sqrt(rotors_thrust/self.C_T)
+        return rotors_speed
+
+    # Consider the limitation of actuator
+    def compute_relaxed_des_rpm(self, f, M, rotor_speed_prev, dt):
+        u = np.hstack([f,M])
+        rotors_thrust = self.K_inv @ u
+        rotors_thrust = self.clamp(rotors_thrust)
+        rotors_speed = np.sqrt(rotors_thrust/self.C_T)
+
+        for i in range(len(rotors_speed)):
+            if rotors_speed[i] - rotor_speed_prev[i] >= dt*self.acc_max:
+                rotors_speed[i] = rotor_speed_prev[i] + dt*self.acc_max
+            elif rotors_speed[i] - rotor_speed_prev[i] < -dt*self.acc_min:
+                rotors_speed[i] = rotor_speed_prev[i] - dt*self.acc_min
+
         return rotors_speed
 
     def clamp(self, rotors_thrust):
