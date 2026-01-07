8fc9116c7c2d06fdc13afc63fb79e862a2f54d9c
kay <kay1216@yonsei.ac.kr>
2026-01-07 15:30:38 +0900

    odom in name modified in dob

--- drone_dob/launch/hgdo.launch.py
+++ drone_dob/launch/hgdo.launch.py
@@ -20,7 +20,7 @@ def generate_launch_description():
         name='hgdo_node',
         output='screen',
         parameters=[config_file_path,
-                    {'use_sim_time': True}]
+                    {'use_sim_time': False}]
     )
 
     # Create and return the LaunchDescription
