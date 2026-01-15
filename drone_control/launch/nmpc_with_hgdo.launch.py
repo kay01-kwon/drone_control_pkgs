15da0a6e586822c6c86a16ceec55dba219346dc9
kay <kay1216@yonsei.ac.kr>
2026-01-16 00:42:23 +0900

    nmpc + hgdo and nmpc + l1 launch file added

---
+++ drone_control/launch/nmpc_with_hgdo.launch.py
@@ -0,0 +1,25 @@
+import os
+
+from ament_index_python.packages import get_package_share_directory
+
+from launch import LaunchDescription
+from launch_ros.actions import Node
+
+def generate_launch_description():
+
+    pkg_prj_drone_control = get_package_share_directory('drone_control')
+
+    config_file = os.path.join(pkg_prj_drone_control, 'config', 'nmpc_with_hgdo.yaml')
+
+    nmpc_with_hgdo_node = Node(
+        package='drone_control',
+        executable='nmpc_with_dob',
+        name='nmpc_with_hgdo',
+        output='screen',
+        parameters=[
+            config_file,
+            {'use_sim_time': False},
+        ]
+    )
+
+    return LaunchDescription([nmpc_with_hgdo_node])
